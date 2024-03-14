222990e619f34d9a2b9647df72e45800248066ed
niefortunny <1niefortunny@gmail.com>
2024-03-14 22:41:40 +0100

    v1.0.1
    Config support

--- AdminSpec.cs
+++ AdminSpec.cs
@@ -1,38 +1,64 @@
+using System.Text.Json.Serialization;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Admin;
-using Microsoft.Extensions.Localization;
+using CounterStrikeSharp.API.Modules.Commands;
 
-namespace AdminSpec
+namespace AdminSpec;
+
+public class AdminSpecConfig : BasePluginConfig
 {
-    public class AdminSpec : BasePlugin
+    [JsonPropertyName("flag")]
+    public string Flag { get; set; } = "@css/ban";
+}
+
+public class AdminSpec : BasePlugin, IPluginConfig<AdminSpecConfig>
+{
+    public override string ModuleName => "AdminSpec";
+    public override string ModuleVersion => "1.0.1";
+    public override string ModuleAuthor => "unfortunate";
+
+    public AdminSpecConfig Config { get; set; } = new();
+
+    public override void Load(bool hotReload)
     {
-        public override string ModuleName => "AdminSpec";
-        public override string ModuleVersion => "1.0.0";
-        public override string ModuleAuthor => "unfortunate";
-
-        public override void Load(bool hotReload)
-        {
-            AddCommandListener("jointeam", OnPlayerJoinTeam, HookMode.Pre);
-        }
-
-        private HookResult OnPlayerJoinTeam(CCSPlayerController player, CommandInfo info)
-        {
-            if (player == null) return HookResult.Continue;
-
-            var desiredTeam = info.ArgByIndex(1);
-            if(desiredTeam == "1") {
-                bool hasPermission = AdminManager.PlayerHasPermissions(player, "@css/ban");
-                if(!hasPermission)
-                {
-                    // TODO: Add sound
-                    player.PrintToChat(Localizer["OnlyAdmin"]);
-                    return HookResult.Handled;
-                }
-            }
-            
-            return HookResult.Continue;
-        }
+        AddCommandListener("jointeam", OnPlayerJoinTeam, HookMode.Pre);
     }
+
+    public void OnConfigParsed(AdminSpecConfig config)
+    {
+        Config = config;
+    }
+
+    private HookResult OnPlayerJoinTeam(CCSPlayerController? player, CommandInfo info)
+    {
+        if (!IsPlayerValid(player))
+            return HookResult.Continue;
+
+        var desiredTeam = info.ArgByIndex(1);
+        if (desiredTeam == "1")
+        {
+            if (!AdminManager.PlayerHasPermissions(player, Config.Flag))
+            {
+                // TODO: Add sound
+                player?.PrintToChat(Localizer["OnlyAdmin"]);
+                return HookResult.Handled;
+            }
+        }
+
+        return HookResult.Continue;
+    }
+
+    #region Helpers
+    public static bool IsPlayerValid(CCSPlayerController? player)
+    {
+        return player != null
+            && player.IsValid
+            && !player.IsBot
+            && player.Pawn != null
+            && player.Pawn.IsValid
+            && player.Connected == PlayerConnectedState.PlayerConnected
+            && !player.IsHLTV;
+    }
+    #endregion
 }
